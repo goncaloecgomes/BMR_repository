93a3bab7238b2c1c0cc65273a6eb0a6c0296a8ba
catarina-p <92151172+catarina-p@users.noreply.github.com>
2021-12-10 11:43:53 +0100

    Add files via upload
    Missing shortest path on the main, made some changes to Global_Nav

--- Global_Nav.py
+++ Global_Nav.py
@@ -9,7 +9,7 @@ from pyzbar.pyzbar import decode
 
 class Global_Nav():
 
-    def __init__(self, client, node, cruising, time_step, Kp, Ki, Kd, error_sum, previous_error, r, L, omega_to_motor, path, R, Q, A, X0, P0):
+    def __init__(self, client, node, cruising, time_step, Kp, Ki, Kd, error_sum, previous_error, r, L, omega_to_motor, R, Q, A, P0):
         self.motor_cruising = np.array([cruising, cruising])
         self.K = np.array([Kp, Ki, Kd])
         self.r = r
@@ -19,18 +19,16 @@ class Global_Nav():
         self.Q = Q
         self.A = A
         self.P_est = P0
-        self.X = X0
         self.client = client
-        self.path = path
         self.ts = time_step
         self.error_sum = error_sum
         self.previous_error = previous_error
         self.node = node 
 
 
-    def PIDcontroller(self, phi_d):
+    def PIDcontroller(self, phi_d, phi):
         """ PID controller """
-        error = phi_d - self.X[0,5]
+        error = phi_d - phi
         self.error_sum += error
         error_dif = error - self.previous_error
         self.previous_error = error
@@ -63,11 +61,11 @@ class Global_Nav():
         return cov_matrix
 
 
-    def kalman_filter(self):
+    def kalman_filter(self, X):
         """takes the previous state (position and velocity in x and y) and covariance matrices  and uses the current sensor\
         data to update the state and covariance matrices i.e. get the robots current location"""
         # Use the previous state to predict the new state
-        X_est_a_priori = np.dot(self.A, self.X)
+        X_est_a_priori = np.dot(self.A, X)
 
         # Use the previous covariance to predict the new covariance
         P_est_a_priori = np.dot(self.A, np.dot(self.P_est, self.A.T))
@@ -85,9 +83,9 @@ class Global_Nav():
         K = np.dot(P_est_a_priori, np.dot(H.T, np.linalg.inv(S)))
 
         # Update the state and covariance matrices with the current sensor data
-        self.X = X_est_a_priori + np.dot(K, i)
+        X = X_est_a_priori + np.dot(K, i)
         self.P_est = P_est_a_priori - np.dot(K, np.dot(H, P_est_a_priori))
-        return 
+        return X
 
 
     def find_thymio(self, img, pixel_to_cm):
@@ -134,30 +132,11 @@ class Global_Nav():
             thymio_vec = thymio_xy_pix - mid_point_back
             phi = np.arctan(thymio_vec[1]/thymio_vec[0])
             thymio_pose = np.array([thymio_xy[0], thymio_xy[1], phi])
-
-            return thymio_xy_pix, thymio_pose
-
-
-    def update_states(self):
-        motor_sensor = self.get_sensor_data()
-        speed_sensor = motor_sensor/self.omega_to_motor
-        # if camera:
-        #   self.X[2,0] = self.thymio_pose[0]                                               # x
-        #   self.X[4,0] = self.thymio_pose[1]                                               # y
-        #   self.X[6,0] = self.thymio_pose[2]                                               # phi
-        #   self.X[5,0] = (self.r/self.L)*(speed_sensor[1]-speed_sensor[0])                 # phi_dot
-        #   self.X[1,0] = (self.r/2)*(speed_sensor[0]+speed_sensor[1])*np.cos(self.X[6,0])  # x_dot
-        #   self.X[3,0] = (self.r/2)*(speed_sensor[0]+speed_sensor[1])*np.sin(self.X[6,0])  # y_dot
-        # else:
-        # X = np.array([[xdot], [x], [ydot], [y], [phi_dot], [phi]])
-        self.X[5,0] = (self.r/self.L)*(speed_sensor[1]-speed_sensor[0])                 # phi_dot
-        self.X[6,0] = self.X[6,0] + self.X[5,0]*self.ts                                 # phi
-        self.X[1,0] = (self.r/2)*(speed_sensor[0]+speed_sensor[1])*np.cos(self.X[6,0])  # x_dot
-        self.X[3,0] = (self.r/2)*(speed_sensor[0]+speed_sensor[1])*np.sin(self.X[6,0])  # y_dot
-        self.X[2,0] = self.X[2,0] + self.X[1,0]*self.ts                                 # x
-        self.X[4,0] = self.X[4,0] + self.X[3,0]*self.ts                                 # y
-        self.kalman_filter()
-        return 
+        else:
+            thymio_xy_pix = 0
+            thymio_pose = 0
+            qr_loc = 0
+        return thymio_xy_pix, thymio_pose, qr_loc
 
 
     def motors(self, motor_left, motor_right):
@@ -166,24 +145,5 @@ class Global_Nav():
             "motor.right.target": [int(motor_right)],
         }
 
-
-    def actuation(self, next_point):
-        phi_d = np.artcan((self.path[next_point].y-self.X[4,0])/(self.path[next_point].x-self.X[2,0]))
-        vr, vl = self.PIDcontroller(phi_d)
-        motor_left = vl*self.omega_to_motor
-        motor_right = vr*self.omega_to_motor
-        
-        # Saturation
-        if motor_left > 500:
-            motor_left = 500
-        elif motor_right > 500:
-            motor_right = 500
-        elif motor_right < -500:
-            motor_right = -500
-        elif motor_left < -500:
-            motor_left = -500
-        
-        adjust_speed = self.motors(motor_left, motor_right)
-        self.node.send_set_variables(adjust_speed)
 
 
